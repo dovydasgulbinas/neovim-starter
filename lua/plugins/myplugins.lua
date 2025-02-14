e9d1e71161af0611bafc09d2041a619977a982eb
Dovydas Gulbinas <dovydas.gulbinas@protonmail.com>
2025-02-14 16:37:33 +0200

    sync

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -1,160 +1,150 @@
 local plugins = {
 
-	{
-		"folke/zen-mode.nvim",
-		lazy = false,
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "vue-language-server",
+        "typescript-language-server",
+        "yamlfmt",
+        "yamllint",
+        "black",
+        "debugpy",
+        "pyright",
+        "html-lsp",
+        "lua-language-server",
+        "stylua",
+        "bash-language-server",
+        -- "docker-compose-language-service",
+        -- "dockerfile-language-server",
+      },
+    },
+  },
 
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"vue-language-server",
-				"typescript-language-server",
-				"yamlfmt",
-				"yamllint",
-				"black",
-				"debugpy",
-				"pyright",
-				"html-lsp",
-				"lua-language-server",
-				"stylua",
-				"bash-language-server",
-				-- "docker-compose-language-service",
-				-- "dockerfile-language-server",
-			},
-		},
-	},
+  -- {
+  -- 	"nvimtools/none-ls.nvim",
+  -- 	lazy = false,
+  -- 	ft = {
+  -- 		"python",
+  -- 		"javascript",
+  -- 		"lua",
+  -- 		"yaml",
+  -- 		"typescript",
+  -- 		"javascript",
+  -- 		"javascriptreact",
+  -- 		"typescriptreact",
+  -- 		"vue",
+  -- 		"json",
+  -- 		"html",
+  -- 		"css",
+  -- 		"nix",
+  -- 	},
+  -- 	opts = function()
+  -- 		return require("configs.none-ls")
+  -- 	end,
+  -- },
 
-	-- {
-	-- 	"nvimtools/none-ls.nvim",
-	-- 	lazy = false,
-	-- 	ft = {
-	-- 		"python",
-	-- 		"javascript",
-	-- 		"lua",
-	-- 		"yaml",
-	-- 		"typescript",
-	-- 		"javascript",
-	-- 		"javascriptreact",
-	-- 		"typescriptreact",
-	-- 		"vue",
-	-- 		"json",
-	-- 		"html",
-	-- 		"css",
-	-- 		"nix",
-	-- 	},
-	-- 	opts = function()
-	-- 		return require("configs.none-ls")
-	-- 	end,
-	-- },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    lazy = false,
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  },
 
-	{
-		"nvim-telescope/telescope-file-browser.nvim",
-		lazy = false,
-		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-	},
+  -- {
+  -- 	"NeogitOrg/neogit",
+  -- 	dependencies = {
+  -- 		"nvim-lua/plenary.nvim", -- required
+  -- 		"sindrets/diffview.nvim", -- optional - Diff integration
+  -- 		-- Only one of these is needed.
+  -- 		"nvim-telescope/telescope.nvim", -- optional
+  -- 	},
+  -- 	config = function()
+  -- 		require("neogit").setup({
+  -- 			-- Optional configuration
+  -- 			integrations = {
+  -- 				diffview = true, -- Optional: Integrates with the `diffview.nvim` plugin
+  -- 			},
+  -- 		})
+  -- 	end,
+  -- },
+  {
+    "NeogitOrg/neogit",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true,
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    opts = {
+      position = "right",
+    },
+  },
+  {
+    "chentoast/marks.nvim",
+    lazy = false,
+    config = function()
+      require("marks").setup()
+    end,
+  },
+  {
+    "tpope/vim-obsession",
+    lazy = false,
+  },
 
-	-- {
-	-- 	"NeogitOrg/neogit",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim", -- required
-	-- 		"sindrets/diffview.nvim", -- optional - Diff integration
-	-- 		-- Only one of these is needed.
-	-- 		"nvim-telescope/telescope.nvim", -- optional
-	-- 	},
-	-- 	config = function()
-	-- 		require("neogit").setup({
-	-- 			-- Optional configuration
-	-- 			integrations = {
-	-- 				diffview = true, -- Optional: Integrates with the `diffview.nvim` plugin
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
-	{
-		"NeogitOrg/neogit",
-		lazy = false,
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- required
-			"sindrets/diffview.nvim", -- optional - Diff integration
-			-- Only one of these is needed.
-			"nvim-telescope/telescope.nvim", -- optional
-		},
-		config = true,
-	},
-	{
-		"simrat39/symbols-outline.nvim",
-		cmd = "SymbolsOutline",
-		keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-		opts = {
-			position = "right",
-		},
-	},
-	{
-		"chentoast/marks.nvim",
-		lazy = false,
-		config = function()
-			require("marks").setup()
-		end,
-	},
-	{
-		"tpope/vim-obsession",
-		lazy = false,
-	},
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "nvchad.configs.lspconfig"
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      -- require("core.utils").load_mappings "dap"
+    end,
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("nvchad.configs.lspconfig")
-			require("configs.lspconfig")
-		end,
-	},
-	{
-		"mfussenegger/nvim-dap",
-		config = function(_, opts)
-			require("core.utils").load_mappings("dap")
-		end,
-	},
-
-	{
-		"mfussenegger/nvim-dap-python",
-		ft = "python",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"rcarriga/nvim-dap-ui",
-		},
-		config = function(_, opts)
-			require("dap-python").setup("uv")
-			require("core.utils").load_mappings("dap_python")
-		end,
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio",
-		},
-		config = function()
-			local dap = require("dap")
-			local dapui = require("dapui")
-			dapui.setup()
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
-		end,
-	},
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, opts)
+      require("dap-python").setup "uv"
+      -- require("core.utils").load_mappings "dap_python"
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require "dap"
+      local dapui = require "dapui"
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
 }
 
 return plugins
