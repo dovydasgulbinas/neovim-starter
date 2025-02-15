7236aae5ba2498a2312ac55af0a02105ac85c0fd
Dovydas Gulbinas <dovydas.gulbinas@protonmail.com>
2025-02-15 08:57:37 +0200

    remove myinit script

--- lua/myinit.lua
+++
@@ -1,8 +0,0 @@
-vim.o.autoread = true
-vim.opt.number = false -- Disable line numbers
-vim.opt.relativenumber = false -- Disable relative line numbers
-
-vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
-	pattern = "*",
-	command = "checktime",
-})
