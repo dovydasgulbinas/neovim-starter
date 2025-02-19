514cb088b6163d9eba17a17488e1d86159e4c531
Dovydas Gulbinas <dovydas.gulbinas@protonmail.com>
2025-02-19 16:13:41 +0200

    update edit script

--- edit.sh
+++ edit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 pushd "$HOME/.config/nvim"
+git pull
 
 nvim .
 
@@ -8,10 +9,17 @@ echo "Starting Neovim Again!"
 
 nvim .
 
+if git diff --quiet; then
+    echo "No changes detected, exiting."
+    popd
+    exit 0
+fi
+
 git add -i 
 
 git commit
 
+git push
 popd
 
 
